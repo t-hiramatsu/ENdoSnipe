fb8408b209f45a39fab18d5ba183b2b6768ed45c
Nobuhiro Kawasaki <kwsk2nbhr@gmail.com>
2013-05-04 15:44:40 +0900

    ツリーメニュー定数クラスの追加

---
+++ Dashboard/src/main/java/jp/co/acroquest/endosnipe/web/dashboard/constants/TreeMenuConstants.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2004-2013 Acroquest Technology Co., Ltd. All Rights Reserved.
+ * Please read the associated COPYRIGHTS file for more details.
+ *
+ * THE  SOFTWARE IS  PROVIDED BY  Acroquest Technology Co., Ltd., WITHOUT  WARRANTY  OF
+ * ANY KIND,  EXPRESS  OR IMPLIED,  INCLUDING BUT  NOT LIMITED  TO THE
+ * WARRANTIES OF  MERCHANTABILITY,  FITNESS FOR A  PARTICULAR  PURPOSE
+ * AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * CLAIM, DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+package jp.co.acroquest.endosnipe.web.dashboard.constants;
+
+/**
+ * ツリーメニューの定数クラス
+ * @author kawasaki
+ *
+ */
+public interface TreeMenuConstants
+{
+
+    /** ツリーメニューのタイプ:グループ(複数の計測対象のグループ) */
+    public static final String TREE_MENU_TYPE_GROUP = "group";
+
+    /** ツリーメニューのタイプ:ターゲット(計測対象となるターゲット) */
+    public static final String TREE_MENU_TYPE_TARGET = "target";
+
+    /** ツリーメニューのタイプ:シグナル */
+    public static final String TREE_MENU_TYPE_SIGNAL = "signal";
+}
