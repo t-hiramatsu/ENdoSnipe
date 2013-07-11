8a1824c29a8c11bb666e95ec60d2d568a72c7d82
eripong <eripong@gmail.com>
2013-07-11 09:49:28 +0900

    5.0.3-003ビルド用ファイルコミット

--- build.ps1
+++ build.ps1
@@ -14,13 +14,6 @@ function global:Replace-Content
 #タグ名称を設定する。
 $tags = "5.0.4-003"
 
-$tag_array = $tags -split "-"
-$ver = $tag_array[0]
-$build = $tag_array[1]
-
-Replace-Content ENdoSnipe\build.bat "set VER=.+" "set VER=$ver"
-Replace-Content ENdoSnipe\build.bat "set BUILD=.+" "set BUILD=$build"
-
 $WorkDir="build"
 
 #tagを作成する。
@@ -42,6 +35,16 @@ $zip = $shell.NameSpace($file)
 $dest =  $shell.NameSpace((Split-Path $file -Parent))
 
 $dest.CopyHere($zip.Items()) 
+
+# バージョンを更新する。
+$tag_array = $tags -split "-"
+$ver = $tag_array[0]
+$build = $tag_array[1]
+
+Replace-Content ENdoSnipe\build.bat "set VER=.+" "set VER=$ver"
+Replace-Content ENdoSnipe\build.bat "set BUILD=.+" "set BUILD=$build"
+
+
 
 # ビルドを実行する。
 cd ENdoSnipe
